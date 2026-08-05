b726350ea12e6288ed3f2655ae528c500f75f76d
Kaetaeru <131461278+Kaetaeru@users.noreply.github.com>
2026-08-05 17:52:03 +0900

    style: match theme applicator formatting

--- implementation/roblox/src/StarterGui/RVTT/UI/ThemeApplicator.lua
+++ implementation/roblox/src/StarterGui/RVTT/UI/ThemeApplicator.lua
@@ -44,11 +44,7 @@ local function lookup(colors: ColorMap, value: any): Color3?
 end
 
 local function applyText(instance: Instance, color: Color3)
-	if
-		instance:IsA("TextLabel")
-		or instance:IsA("TextButton")
-		or instance:IsA("TextBox")
-	then
+	if instance:IsA("TextLabel") or instance:IsA("TextButton") or instance:IsA("TextBox") then
 		(instance :: any).TextColor3 = color
 	end
 end
